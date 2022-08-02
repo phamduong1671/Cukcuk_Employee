e4c769cc8cc1c1d585adfbe3177a568539bf7a22
phamduong1671 <74924072+phamduong1671@users.noreply.github.com>
2022-08-02 20:55:56 +0700

    custom combobox

--- js/combobox.js
+++ js/combobox.js
@@ -3,18 +3,34 @@
  * Hàm dùng để ẩn hiện giá combobox-value
  * PA_Dương (01/08/2022)
  */
- function iconDropDown(){
+ function iconDropDown(id){
     let me = this;
-    let cbbValueClass;
+    let cbbValueClass, a, b, c, d;
 
-    cbbValueClass = document.getElementById("combobox-value").className;
+    if(id == "icon-dropdown1"){
+        a = "combobox-value1";
+        b = "icon-cbb1";
+        c = "combobox-value2";
+        d = "icon-cbb2";
+    }
+    if(id == "icon-dropdown2"){
+        a = "combobox-value2";
+        b = "icon-cbb2";
+        c = "combobox-value1";
+        d = "icon-cbb1";
+
+    }
+
+    cbbValueClass = document.getElementById(a).className;
     if(cbbValueClass == "combobox-value"){
-        document.getElementById("combobox-value").classList.add("combobox-hide");
-        document.getElementById("icon-cbb").className = "fas fa-angle-down";
+        document.getElementById(a).classList.add("combobox-hide");
+        document.getElementById(b).className = "fas fa-angle-down";
     }
     else{
-        document.getElementById("combobox-value").classList.remove("combobox-hide");
-        document.getElementById("icon-cbb").className = "fas fa-angle-up";
+        document.getElementById(a).classList.remove("combobox-hide");
+        document.getElementById(b).className = "fas fa-angle-up";
+        document.getElementById(c).classList.add("combobox-hide");
+        document.getElementById(d).className = "fas fa-angle-down";
     }
 }
 
@@ -24,8 +40,19 @@
  */
 function comboboxItem(id){
     let me = this;
+    let a, b, c;
+    c = document.getElementById(id).parentNode.id;
 
-    document.getElementById("input-combobox").value = document.getElementById(id).innerHTML;
-    document.getElementById("combobox-value").classList.add("combobox-hide");
-    document.getElementById("icon-cbb").className = "fas fa-angle-down";
+    if(c == "combobox-value1"){
+        a = "input-combobox1";
+        b = "icon-cbb1";
+    }
+    if(c == "combobox-value2"){
+        a = "input-combobox2";
+        b = "icon-cbb2";
+    }
+
+    document.getElementById(a).value = document.getElementById(id).innerHTML;
+    document.getElementById(c).classList.add("combobox-hide");
+    document.getElementById(b).className = "fas fa-angle-down";
 }
